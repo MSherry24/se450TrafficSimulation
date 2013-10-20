3e228dc2ce530f374d850c8d8b917bab9a547b60
Mike Sherry <mikesherry24@gmail.com>
2013-10-20 18:52:19 -0500

    Added car list structure to Road class

--- src/Model/Road.java
+++ src/Model/Road.java
@@ -1,9 +1,18 @@
 package Model;
 
+import java.util.List;
+
 public class Road {
+	
+	private List<Car> carsOnThisRoad;
 	
 	public Road() {
 		
+	}
+	
+	public void add(Car carToAdd) {
+		if (carToAdd == null)  throw (new IllegalArgumentException());
+		carsOnThisRoad.add(carToAdd);
 	}
 	
 	
